1f89ce5d70b4074561e9326742cbec665a848c1b
Jaesin Mulenex <jaesin@no-reply.gradientsky.com>
2019-08-12 17:36:53 -0700

    Add a trait for creating management entities.

---
+++ src/ApigeeSdk/ManagementSdkEntityCreationTrait.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\MockClient\ApigeeSdk;
+
+use Apigee\Edge\Api\Management\Controller\OrganizationController;
+use Apigee\Edge\Api\Management\Entity\Organization;
+use Apigee\MockClient\Generator\ApigeeSdkEntitySource;
+use Http\Message\RequestMatcher\RequestMatcher;
+
+/**
+ * A helper trait for generating responses for Apigee SDK entities.
+ */
+trait ManagementSdkEntityCreationTrait {
+
+  /**
+   * The Apigee SDK client.
+   *
+   * @var \Apigee\Edge\ClientInterface
+   */
+  protected $sdkClient;
+
+  /**
+   * The mock client.
+   *
+   * @var \Apigee\MockClient\MockClient
+   */
+  protected $mockClient;
+
+  /**
+   * The response factory with the `ApigeeSdkEntityGenerator` added.
+   *
+   * @var \Apigee\MockClient\ResponseFactoryInterface
+   */
+  protected $responseFactory;
+
+  public function createOrganizationSdkEntity(array $values = []) {
+    // Create the organization.
+    $organization = new Organization($values);
+    // This covers getting, updating and deleting an organization but not
+    // create. A queued response can cover that.
+    $this->mockClient->on(
+      new RequestMatcher("/v1/organizations/{$organization->id()}", NULL, ['GET', 'PUT', 'DELETE']),
+      $this->responseFactory->generateResponse(new ApigeeSdkEntitySource($organization))
+    );
+
+    // Return the loaded entity.
+    $controller = new OrganizationController($this->sdkClient);
+    return $controller->load($organization->id());
+  }
+
+}
---
+++ tests/ApigeeSdk/ManagementSdkEntityCreationTraitTest.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Apigee\MockClient\Tests\ApigeeSdk;
+
+use Apigee\Edge\Api\Management\Controller\OrganizationController;
+use Apigee\Edge\Structure\PropertiesProperty;
+use Apigee\MockClient\ApigeeSdk\ManagementSdkEntityCreationTrait;
+use Apigee\MockClient\ApigeeSdk\SdkMockClient;
+use Apigee\MockClient\Generator\ApigeeSdkEntityGenerator;
+use Apigee\MockClient\MockClient;
+use Apigee\MockClient\ResponseFactory;
+use Apigee\MockClient\Tests\RandomStringGeneratorTrait;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Tests Entity response generation.
+ */
+class ManagementSdkEntityCreationTraitTest extends TestCase {
+
+  use ManagementSdkEntityCreationTrait;
+  use RandomStringGeneratorTrait;
+
+  public function setUp() {
+    parent::setUp();
+
+    $this->mockClient = new MockClient();
+    $this->sdkClient = new SdkMockClient($this->mockClient);
+    $this->responseFactory = new ResponseFactory();
+    $this->responseFactory->addGenerator(new ApigeeSdkEntityGenerator());
+  }
+
+  /**
+   * Tests the organization creator trait.
+   */
+  public function testCreateSdkOrganization() {
+    $organization = $this->createOrganizationSdkEntity([
+      'name' => $this->randomCharacters(),
+      'properties' => [new PropertiesProperty(["name" => "subscriptionType","value" => "enterprise"])],
+    ]);
+
+    $organization_controller = new OrganizationController($this->sdkClient);
+
+    static::assertSame('GET', $this->mockClient->getLastRequest()->getMethod());
+
+    $organization_controller->update($organization);
+    $organization_controller->delete($organization->id());
+  }
+
+}
